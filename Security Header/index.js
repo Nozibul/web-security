bdafd4a27b10a3ba06da0a419c52bbaabcf42b73
Nozibul <nozibulislamspi@gmail.com>
2025-05-14 18:02:50 +0600

    chore: enforce HTTPS and manually set security headers (HSTS, CSP, Referrer-Policy, etc.)

--- Security Header/index.js
+++ Security Header/index.js
@@ -3,9 +3,21 @@ const app = express()
 const port = 3000;
 // app.disable('x-powered-by')
 
+const redirectToHttps = (req, res, next) => {
+  if(req.headers['x-forwarded-proto'] !== 'https') {
+    // Redirect to https
+    return res.redirect('https://' + req.hostname + req.url);
+  }
+  next();
+}
+app.use(redirectToHttps);
 // manually remove header
 app.use((req, res, next) => {
+    res.setHeader("Referer-Policy", "no-referrer");
     res.removeHeader("X-Powered-By");
+    res.setHeader("X-Content-Type-Options", "nosniff");
+    res.setHeader("Content-Security-Policy", "default-src 'self'");
+    res.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
     next();
 });
 
